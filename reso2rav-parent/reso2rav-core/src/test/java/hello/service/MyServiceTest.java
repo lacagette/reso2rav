8b705cda67b147a2b128edfeadf382836f791595
carlitto <carlitto@debian-debian-debian>
2018-01-27 21:33:53 +0100

    Suppression hello world

--- reso2rav-parent/reso2rav-core/src/test/java/hello/service/MyServiceTest.java
+++ reso2rav-parent/reso2rav-core/src/test/java/hello/service/MyServiceTest.java
@@ -1,10 +1,6 @@
 package hello.service;
 
-import static org.assertj.core.api.Assertions.*;
-
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,16 +9,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest("service.message=Hello")
 public class MyServiceTest {
 
-    @Autowired
-    private MyService myService;
+	// @Autowired
+	// private MyService myService;
+	//
+	// @Test
+	// public void contextLoads() {
+	// assertThat(myService.message()).isNotNull();
+	// }
 
-    @Test
-    public void contextLoads() {
-        assertThat(myService.message()).isNotNull();
-    }
-
-    @SpringBootApplication
-    static class TestConfiguration {
-    }
+	@SpringBootApplication
+	static class TestConfiguration {
+	}
 
 }
