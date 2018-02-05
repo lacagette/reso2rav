42143d526f32e9d8f348e1b9f7aaa71fe9256930
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2018-02-05 18:00:08 +0100

    Ajout du rootReducer && WIP sur ProducersList

--- reso2rav-parent/reso2rav-front/src/index.js
+++ reso2rav-parent/reso2rav-front/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Store from './store';
+import * as Provider from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const StoreInstance = Store();
+
+ReactDOM.render(
+    <Provider store={StoreInstance}>
+    <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
---
+++ reso2rav-parent/reso2rav-front/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+const rootReducer = combineReducers({
+    // Todo: Ajouter ici les reducers pour chaque composant
+});
+export default rootReducer;
