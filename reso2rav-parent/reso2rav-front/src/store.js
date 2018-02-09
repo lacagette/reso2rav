d1c09316a089c311f8f9ac0ae4f5aebf429e366a
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2018-02-09 13:27:49 +0100

    Correction de l'init des devTools et recup du state

--- reso2rav-parent/reso2rav-front/src/store.js
+++ reso2rav-parent/reso2rav-front/src/store.js
@@ -1,13 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from  './reducers';
 import initialState  from './initialState';
+import producersReducer from './reducers/producersReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default() => {
     return createStore(
-        rootReducer,
+        //rootReducer,
+        producersReducer,
         initialState,
-        applyMiddleware(thunkMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        composeEnhancers(
+            applyMiddleware(thunkMiddleware)
+        )
     );
 }
