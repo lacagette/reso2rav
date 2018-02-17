0b8e7331fc35d4b395cab8614ef20294dac48b37
BetterWork4BetterWorld <galois@riseup.net>
2018-02-18 00:21:14 +0100

    Déclaration properties avec @ConfigurationProperty et création property environnement

--- reso2rav-parent/reso2rav-web/src/main/java/fr/mouvement/reso2rav/web/configuration/SpringRepositoryRestConfiguration.java
+++ reso2rav-parent/reso2rav-web/src/main/java/fr/mouvement/reso2rav/web/configuration/SpringRepositoryRestConfiguration.java
@@ -1,32 +1,28 @@
 package fr.mouvement.reso2rav.web.configuration;
 
-import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.stereotype.Component;
 
+import fr.mouvement.reso2rav.web.configuration.Reso2RavWebProperties.Environnement;
+
 @Component
 public class SpringRepositoryRestConfiguration extends RepositoryRestConfigurerAdapter {
 
+	@Autowired
+	Reso2RavWebProperties reso2RavWebProperties;
+
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-		config.setRepositoryDetectionStrategy(RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED);
-		config.getCorsRegistry()
-				.addMapping("/**") //
-				.allowedOrigins("*") //
-				.allowedHeaders("*") //
-				.allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+		if (reso2RavWebProperties.geEnvironnementEnum() == Environnement.LOCAL) {
+			config.setRepositoryDetectionStrategy(RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED);
+			config.getCorsRegistry().addMapping("/**") //
+					.allowedHeaders("*") //
+					.allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+					.allowedOrigins("http://localhost:3000");
+		}
 	}
-
-//	public class NONE_BY_DEFAULT implements RepositoryDetectionStrategy {
-//
-//		@Override
-//		public boolean isExported(RepositoryMetadata metadata) {
-//			// TODO Auto-generated method stub
-//			return false;
-//		}
-//
-//	}
 
 }
