579304a62956ad7a1b7581e7afb90383a857f6a4
BetterWork4BetterWorld <galois@riseup.net>
2018-02-18 22:08:21 +0100

    Mise en place des tests avec base mongo embarquée

---
+++ reso2rav-parent/reso2rav-core/src/test/java/fr/mouvement/reso2rav/core/ApplicationTest.java
@@ -0,0 +1,13 @@
+package fr.mouvement.reso2rav.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTest.class, args);
+	}
+
+}
---
+++ reso2rav-parent/reso2rav-core/src/test/java/fr/mouvement/reso2rav/core/domain/MongoLazyLoadingTest.java
@@ -0,0 +1,41 @@
+package fr.mouvement.reso2rav.core.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fr.mouvement.reso2rav.core.repository.ProducteurRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongoLazyLoadingTest {
+
+	@Autowired
+	ProducteurRepository producteurRepository;
+
+	@Before
+	public void init() {
+
+		Producteur producteur = new Producteur();
+
+		producteur.getIdentiteAvecAdresse().setNom("Gentil producteur");
+
+		producteurRepository.save(producteur);
+
+	}
+
+	@Test
+	public void lazyLoadingTest() {
+
+		Producteur producteur = producteurRepository.findAll().iterator().next();
+
+		assertEquals("Le reseau test", producteur.getIdentiteAvecAdresse().getNom());
+
+	}
+
+}
