2eb9e53e2536a9b63a3f471634d9a0144a15f00f
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2018-03-04 21:57:55 +0100

    WIP

--- reso2rav-parent/reso2rav-front/src/components/ProducersList.js
+++ reso2rav-parent/reso2rav-front/src/components/ProducersList.js
@@ -5,12 +5,19 @@ class ProducersList extends Component{
     constructor(props){
         super(props);
         this.producers=[
-	        { id: 1, firstName: "Bob", lastName: "Léponge", address: "Au fond a droite", products: "Légumes", isDisabled: false},
-	        { id: 2, firstName: "Robert", lastName: "Dacier", address: "Au fond a gauche", products: "Fruits", isDisabled: true}
+	        // { id: 1, firstName: "Bob", lastName: "Léponge", address: "Au fond a droite", products: "Légumes", isDisabled: false},
+	        // { id: 2, firstName: "Robert", lastName: "Dacier", address: "Au fond a gauche", products: "Fruits", isDisabled: true}
 	    ];
     }
 
     render(){
+        if (!this.props.producers) {
+            return(
+                <div>
+                    Vide!!
+                </div>
+            )
+        }
         return(
             <div>
                 <h2>Liste des producteurs:</h2>
