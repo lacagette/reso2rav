4ceff86794dc4319cdf1337e67d32b2bc97f6877
BetterWork4BetterWorld <galois@riseup.net>
2018-03-11 22:39:16 +0100

    Nettoyage de la base embedded après chaque tests

--- reso2rav-parent/reso2rav-web/src/test/java/fr/mouvement/reso2rav/web/repository/AbstractTestWithJeuEssai.java
+++ reso2rav-parent/reso2rav-web/src/test/java/fr/mouvement/reso2rav/web/repository/AbstractTestWithJeuEssai.java
@@ -1,8 +1,10 @@
 package fr.mouvement.reso2rav.web.repository;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import fr.mouvement.reso2rav.core.utils.JeuEssaiUtils;
@@ -13,9 +15,17 @@ public abstract class AbstractTestWithJeuEssai {
 	@Autowired
 	JeuEssaiUtils jeuEssaiUtils;
 
+	@Autowired
+	MongoTemplate mongoTemplate;
+
 	@Before
 	public void creerJeuTest() {
 		jeuEssaiUtils.creerJeuEssai();
+	}
+
+	@After
+	public void cleanUpDatabase() {
+		mongoTemplate.getDb().dropDatabase();
 	}
 
 }
