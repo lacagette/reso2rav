40a2472fccf8c6c5a213ff8eba35d8a7f01ab254
BetterWork4BetterWorld <galois@riseup.net>
2018-03-11 21:38:10 +0100

    Abstract test pour création jeu essai

---
+++ reso2rav-parent/reso2rav-core/src/test/java/fr/mouvement/reso2rav/core/repository/AbstractTestWithJeuEssai.java
@@ -0,0 +1,21 @@
+package fr.mouvement.reso2rav.core.repository;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fr.mouvement.reso2rav.core.utils.JeuEssaiUtils;
+
+@RunWith(SpringRunner.class)
+public abstract class AbstractTestWithJeuEssai {
+
+	@Autowired
+	JeuEssaiUtils jeuEssaiUtils;
+
+	@Before
+	public void creerJeuTest() {
+		jeuEssaiUtils.creerJeuEssai();
+	}
+
+}
