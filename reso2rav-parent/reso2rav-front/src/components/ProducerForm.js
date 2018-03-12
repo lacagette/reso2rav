0432d332bd2fa6036f53c6aef322831eb0998dc5
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2018-03-12 21:20:42 +0100

    Ajout des champs de saisie ProducerForm

--- reso2rav-parent/reso2rav-front/src/components/ProducerForm.js
+++ reso2rav-parent/reso2rav-front/src/components/ProducerForm.js
@@ -9,23 +9,47 @@ class ProducerForm extends Component {
         console.log(producer)
     }
     render() {
+
         return(
             <LocalForm
                 model="producer"
                 onSubmit={(producer) => this.handleSubmit(producer)}
             >
+                {/*Nom exploitation*/}
+                <label htmlFor="producer.nomExploitation">Nom de famille:</label>
+                <Control.text model="producer.nomExploitation" id="producer.nomExploitation" />
+                {/*Prénom*/}
                 <label htmlFor="producer.identite.prenom">Prénom:</label>
                 <Control.text model="producer.identite.prenom" id="producer.identite.prenom" />
-
+                {/*Nom de famille*/}
                 <label htmlFor="producer.identite.nom">Nom de famille:</label>
                 <Control.text model="producer.identite.nom" id="producer.identite.nom" />
-
+                {/*Téléphone*/}
+                <label htmlFor="producer.identite.telephone">N° de téléphone:</label>
+                <Control.text model="producer.identite.telephone" id="producer.identite.telephone" />
+                {/*Email*/}
+                <label htmlFor="producer.mail.nom">Email:</label>
+                <Control.text model="producer.identite.mail" id="producer.identite.mail" />
+                {/*N° de rue*/}
+                <label htmlFor="producer.adresse.numero">N° de rue:</label>
+                <Control.text model="producer.adresse.numero" id="producer.adresse.numero" />
+                {/*Nom de rue*/}
+                <label htmlFor="producer.adresse.nomVoie">Nom de rue:</label>
+                <Control.text model="producer.adresse.nomVoie" id="producer.adresse.nomVoie" />
+                {/*Lieu dit*/}
+                <label htmlFor="producer.adresse.lieuDit">Lieu-dit:</label>
+                <Control.text model="producer.adresse.lieuDit" id="producer.adresse.lieuDit" />
+                {/*Commune*/}
+                <label htmlFor="producer.adresse.commune">Commune:</label>
+                <Control.text model="producer.adresse.commune" id="producer.adresse.commune" />
+                {/*Code postal*/}
+                <label htmlFor="producer.adresse.codePostal">Code postal:</label>
+                <Control.text model="producer.adresse.codePostal" id="producer.adresse.codePostal" />
                 <button type="submit">
                     Enregistrer le producteur
                 </button>
             </LocalForm>
         )
-
     }
 }
 
